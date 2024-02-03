9d2f8d9db4803137a327b1b70d13a44a08a9e4c0
danyal sameer khalife <141676015+Danyalsk@users.noreply.github.com>
2024-02-03 22:43:24 +0530

    Remove default redirect from root path

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,10 +10,6 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      redirect: '/login'
-    },
-    {
       path: '/login',
       name: 'Login',
       component: LoginView
