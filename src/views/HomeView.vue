819db8785ead71f3a9fad9bef0f948d51606e808
danyal sameer khalife <141676015+Danyalsk@users.noreply.github.com>
2024-02-04 00:51:05 +0530

    Remove commented out code for the "Orders" button

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -278,13 +278,13 @@
                       >
                         Add Product
                       </button>
-                      <button
+                      <!-- <button
                         @click="$router.push((name = '/your-order'))"
                         type="button"
                         class="mx-4 flex w-[120px] justify-center rounded-md bg-indigo-600 px-3.5 py-3 text-sm font-semibold text-white shadow-sm"
                       >
                         Orders
-                      </button>
+                      </button> -->
                     </div>
                   </div>
                 </div>
