69321875d243523eed4e919d3d102c4ffd2fd4e6
danyal sameer khalife <141676015+Danyalsk@users.noreply.github.com>
2024-09-05 03:15:39 +0530

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,6 +9,10 @@ const router = createRouter({
   routes: [
     {
       path: '/',
+      redirect: '/home'
+    }
+,    {
+      path: '/',
       name: 'home',
       component: () => import('@/views/HomeView.vue')
     },
